8fed0141c1ebf414a9ade78bdb8e9dfb78f22914
192-sean <192.sean.luke@gmail.com>
2014-03-19 22:27:04 -0500

    with handlebars

--- Cal/app.js
+++ Cal/app.js
@@ -9,12 +9,16 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var exphbs = require('express3-handlebars');
+
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -30,8 +34,11 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/', function(req, res) {
+  res.render('home');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
